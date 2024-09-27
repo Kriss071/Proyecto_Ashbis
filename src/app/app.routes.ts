d075b1b1a7bb7b3037945a188869180a3a9984e1
Kriss071 <chrisquinga@gmail.com>
2024-09-27 17:53:08 -0300

    Componente Perfil de usuario (Incompleto)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UserProfileComponent } from './pages/profile/user-profile/user-profile.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent, pathMatch: 'full'},
-    { path: 'register', component: RegisterComponent, pathMatch: 'full'}
+    { path: 'register', component: RegisterComponent, pathMatch: 'full'},
+
+    { path: 'perfil', component: UserProfileComponent, pathMatch: 'full'}
 ];
